258624e6e9fc81a0cf697caa97e5ce618070bd1d
sineagle <997991067@qq.com>
2023-02-06 16:50:18 +0800

    实现了注册登录功能

--- src/main/java/com/sineagle/controller/UserController.java
+++ src/main/java/com/sineagle/controller/UserController.java
@@ -1,8 +1,13 @@
 package com.sineagle.controller;
 
 import com.sineagle.pojo.Users;
+import com.sineagle.pojo.vo.UsersVO;
+import com.sineagle.service.UserService;
 import com.sineagle.utils.IMoocJSONResult;
+import com.sineagle.utils.MD5Utils;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +16,38 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("u")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
     @PostMapping("/registerOrLogin")
-    public IMoocJSONResult registerOrLogin(@RequestBody Users user) {
+    public IMoocJSONResult registerOrLogin(@RequestBody Users user) throws Exception {
         // 0. 判断用户名和密码不能为空
         if (StringUtils.isBlank(user.getUsername()) || StringUtils.isBlank(user.getPassword())) {
             return IMoocJSONResult.errorMsg("用户名或密码不能为空 ! ");
         }
 
-        return IMoocJSONResult.ok();
+        // 1. 判断用户名是否存在，如果存在则登录，否则进行注册
+        boolean usernameIsExist = userService.queryUsernameIsExist(user.getUsername());
+        Users userResult = null;
+        if (usernameIsExist) {
+            // 1.1 登录
+            userResult = userService.queryUserForLogin(user.getUsername(),
+                    MD5Utils.getMD5Str(user.getPassword()));
+            if (userResult == null) {
+                return IMoocJSONResult.errorMsg("用户名或密码不正确!");
+            }
+        } else {
+            // 1.2 注册
+            user.setNickname(user.getUsername());
+            user.setFaceImage("");
+            user.setFaceImageBig("");
+            user.setPassword(MD5Utils.getMD5Str(user.getPassword()));
+
+            userResult = userService.saveUser(user);
+        }
+
+        UsersVO userVo = new UsersVO();
+        BeanUtils.copyProperties(userResult, userVo);
+        return IMoocJSONResult.ok(userVo);
     }
 }
--- src/main/java/com/sineagle/service/UserService.java
+++ src/main/java/com/sineagle/service/UserService.java
@@ -1,11 +1,22 @@
 package com.sineagle.service;
 
+import com.sineagle.pojo.Users;
+
 public interface UserService {
     /**
      * 判断用户名是否存在
-     * @param username
-     * @return
      */
     public boolean queryUsernameIsExist(String username);
+
+    /**
+     * 查询用户是否存在
+     */
+    public Users queryUserForLogin(String username, String pwd);
+
+
+    /**
+     * 用户注册
+     */
+    public Users saveUser(Users user);
 
 }
